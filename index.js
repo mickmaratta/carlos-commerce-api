080a99f3ffbd22968ac1ba52ad31d08540ddac6b
Mick Maratta <mickmaratta@gmail.com>
2022-10-04 10:31:48 -0400

    Updated PORT in index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
 const stripeRoute = require("./routes/stripe")
 const cors = require("cors");
-
+const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
@@ -27,6 +27,6 @@ app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
-app.listen(5000, ()=> {
-    console.log("Backend server is running")
+app.listen(PORT, ()=> {
+    console.log("Backend server is running at " + PORT)
 })
